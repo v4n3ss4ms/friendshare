736feed0b0a98a79cbbb0ed596b77b7a39e13975
v4n3ss4ms <v4n3ss4ms@gmail.com>
2023-04-08 18:21:28 +0200

    Reset and basics styles. Expenses filled and its styles

--- src/app/expense-item/expense-item.component.ts
+++ src/app/expense-item/expense-item.component.ts
@@ -11,8 +11,19 @@ import { GetUserName } from '../../application/get-user-name';
 export class ExpenseItemComponent implements OnInit {
   @Input() expense = {} as Expense;
   @Input() users = [] as User[];
+  date = '' as string;
 
-  constructor(private getUserName: GetUserName) {}
+  constructor(public getUserName: GetUserName) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const dateOptions: object = {
+      hour12: false,
+      day: '2-digit',
+      month: '2-digit',
+      year: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+    };
+    this.date = new Date(this.expense.date).toLocaleString([], dateOptions);
+  }
 }
