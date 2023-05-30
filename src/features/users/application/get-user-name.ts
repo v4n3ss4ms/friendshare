d843ff360d2701ecca049bb8ded341e07e845f1f
Vanessa <v4n3ss4ms@gmail.com>
2023-05-30 15:07:29 +0200

    inline refactor

--- src/features/users/application/get-user-name.ts
+++ src/features/users/application/get-user-name.ts
@@ -1,14 +1,14 @@
-import { Injectable } from '@angular/core';
-import { User } from '../domain/user';
+import {Injectable} from '@angular/core';
+import {User} from '../domain/user';
+
 @Injectable({
   providedIn: 'root',
 })
 export class GetUserName {
   constructor() {}
 
-  execute(id: number, users: User[]) {  //traer del repo el users
+  execute(id: number, users: User[]) {
     const user: User | undefined = users.find((user) => user.id === id);
-    const name: string = user ? user.name : 'This user no longer exists';
-    return name;
+    return user ? user.name : 'This user no longer exists';
   }
 }
